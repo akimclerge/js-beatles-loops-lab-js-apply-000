0dc8f4fbc154923858f2654a842785a2ea9c845e
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-04-24 01:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ function theBeatlesPlay(muscians, instruments){
 }
 
 function johnLennonFacts(string){
-  return 
-  const johnLennonFacts= ["He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"];
+  
   
 }
