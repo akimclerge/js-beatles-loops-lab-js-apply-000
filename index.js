a1504fc8a436accca0c8e740d0e1d1a8f08d8a29
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-04-24 00:49:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+const musicians= ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments= ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var Neww = []
+
+function theBeatlesPlay(muscians, instruments){
+  var beatlesloop= Neww.concat(muscians, instruments);
+  return beatlesloop
+}
+function johnLennonFacts(string){
+  return 
+  const johnLennonFacts= ["He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"];
+  
+}
