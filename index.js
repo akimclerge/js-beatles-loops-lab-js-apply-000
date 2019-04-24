25b5fd7152ba0aa28a42a627adcf9aecec00ba59
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-04-24 01:27:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(muscians, instruments){
   var musicianSentences = []
   
   for(var i = 0; i < muscians.length; i++){
-    var newSentence = muscians[i] = " plays " + instruments[i] 
+    var newSentence = muscians[i] + " plays " + instruments[i] 
     musicianSentences[i] = newSentence;
   }
   
